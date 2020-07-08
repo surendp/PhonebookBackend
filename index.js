10b37c06d1fe0e5fa4e8a0b3ce86ee981e75e0e2
Surendra Pandey <surendra.pandey2@metropolia.fi>
2020-07-08 15:26:12 +0300

    - Take the port from .env file, if not available set 3001; - Set cors middleware in the express app;

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
-const { json } = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
   return [
@@ -90,7 +91,7 @@ app.post('/api/persons', (request, response) => {
   return response.status(201).end()
 })
 
-const port = 3001
+const port = process.env.PORT || 3001
 app.listen(port, () => {
   console.log(`Server running in port ${port}`)
 })
